77653f65a5578d310470ec0625ad9c4824c0031c
yarec <devnull@localhost>
2014-07-16 16:31:35 +0800

    out nginx conf for php

--- scheme/lnmp.scm
+++ scheme/lnmp.scm
@@ -22,6 +22,25 @@
   (run (phpbrew install 5.5.14 
                 +default +fpm +mysql +pdo))
 
+  (out #<<EOF
+#--------------------------
+# A example conf for nginx
+#--------------------------
+    server {
+        server_name yiicos;
+        root /opt/app/;
+        index index.php index.html index.htm;
+
+        location ~ \.php$ {
+            fastcgi_pass   127.0.0.1:9000;
+            fastcgi_index  index.php;
+            fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+            include        fastcgi_params;
+        }
+    }
+
+EOF
+)
   ;exts memcache memcached pdo_dblib
   )
 
