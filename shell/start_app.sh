ca00f7072d39fe6cb41a3374cd170f42a114517e
yarec <devnull@localhost>
2014-10-21 15:38:38 -0400

    RUN_PATH

--- shell/start_app.sh
+++ shell/start_app.sh
@@ -6,4 +6,5 @@ scsh=/usr/local/bin/scsh
 lp="+lp $scsh_libdir"
 ll=''; for i in $scsh_loads; 
 do ll="$ll -ll $i"; done
-{ $scsh $lp $ll -m $scsh_module -c '(main)' $* 2>&1 1>&3 | 1>&2; } 3>&1 
+$scsh $lp $ll -m $scsh_module -c '(main)' $*
+#{ $scsh $lp $ll -m $scsh_module -c '(main)' $* 2>&1 1>&3 | 1>&2; } 3>&1 
