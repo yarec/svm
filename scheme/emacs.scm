120f95fb8d76a366bd5ad7d607a4fdc001829b77
yarec <devnull@localhost>
2014-12-12 21:37:38 +0800

    fix em on mac

--- scheme/emacs.scm
+++ scheme/emacs.scm
@@ -1,7 +1,10 @@
 
 (define (start-emacs d od)
   (git-clone "https://github.com/purcell/emacs.d" (string-append (home-dir) "/.emacs.d"))
-  (run (emacs --no-window-system)))
+  (if (string=? "mac\n" os-type) 
+    (run (emacs))
+    (run (emacs --no-window-system))
+    ))
 
 (define (emacs d od)
   (get-opt 
