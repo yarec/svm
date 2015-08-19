7f3cd8424a8523fb7ab1e6346dc482e2aaa16e78
yarec <devnull@localhost>
2015-08-19 18:53:45 +0800

    lum -N & -c

--- scheme/luminus.scm
+++ scheme/luminus.scm
@@ -9,6 +9,11 @@
          (appname (if (string=? value "") "lapp" value)))
     (run (lein new luminus ,appname +mysql +site +http-kit))))
 
+(define (new-with-full d od)
+  (let* ((value (oret:value d))
+         (appname (if (string=? value "") "lapp" value)))
+    (run (lein new luminus ,appname +mongodb +auth +cljs +http-kit))))
+
 (define (run-lein-ring d od)
   (run (lein ring server-headless)))
     
@@ -18,14 +23,19 @@
 (define (run-lein-war d od)
   (run (lein ring uberwar)))
 
+(define (run-lein-cljar d od)
+  (run (lein deploy clojars)))
+
 (define (luminus data oret-data)
   (get-opt 
     `(
       (--new-lum      -n|s|t " new luminus               "  ,new-luminus)
+      (--new-lum-full -N|s|t " new luminus with full     "  ,new-with-full)
       (--new-with-m   -m|s|t " new luminus with mysql    "  ,new-with-mysql)
       (--run          -r     " run and watch             "  ,run-lein-ring)
       (--jar          -j     " build exec jar            "  ,run-lein-jar)
       (--war          -w     " build war                 "  ,run-lein-war)
+      (--cljar        -c     " deploy cljar              "  ,run-lein-cljar)
       (--debug        -d||f  " debug                     "  #f)
       (--default      -      " default action            "  ,get-opt-usage)
       (--help         -h     " bprint this usage message "  ,get-opt-usage))))
