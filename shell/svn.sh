330018326f103538735e580b019fde77cb65e1d8
yarec <devnull@localhost>
2015-08-21 17:17:50 +0800

    svn auther

--- shell/svn.sh
+++ shell/svn.sh
@@ -15,15 +15,18 @@ grepstr(){
 }
 svnlist() {
     gstr=$(grepstr)
-
     from_rev=$1
-    path=$2
-    #sed -n '/rentie/,/--$/ p'
-    svn log $path -r $from_rev:HEAD -v|sed -n '/rentie/,/--$/ p'|egrep $gstr|sort|uniq |sed 's@ (from .*@@'|sed 's@^\s*M\s*@.@'|sed 's@^\s*A\s*@.@'|sed '/^\s*D.*$/d'
-    #svn log -r $1 -v|egrep $grepstr|sort|uniq|sed 's@^\s*M\s*@.@'|sed 's@^\s*A\s*@.@'|sed '/^\s*D.*$/d'
+    auther=$2
+    path=$3
+
+    if [ ! -z $auther ]; then
+        svn log $path -r $from_rev:HEAD -v|sed -n "/$auther/,/--$/ p"|egrep $gstr|sort|uniq |sed 's@ (from .*@@'|sed 's@^\s*M\s*@.@'|sed 's@^\s*A\s*@.@'|sed '/^\s*D.*$/d'
+    else
+        svn log $path -r $from_rev:HEAD -v|egrep $gstr|sort|uniq |sed 's@ (from .*@@'|sed 's@^\s*M\s*@.@'|sed 's@^\s*A\s*@.@'|sed '/^\s*D.*$/d'
+    fi
 }
 
-svnlist $1
+svnlist $1 $2
 
 svn_last_rev_file=~/.svm/svn-last-pub-rev
 if [ ! -f $svn_last_rev_file ]; then
