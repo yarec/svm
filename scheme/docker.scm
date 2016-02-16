c64071e4f9318833fc54ac9c9467217ff0e7e24c
yarec <devnull@localhost>
2016-02-16 18:37:20 +0800

    machine-create

--- scheme/docker.scm
+++ scheme/docker.scm
@@ -1,24 +1,29 @@
+(define (default-machine-name d)
+  (or-str (oret:value d) "dev"))
+
 (define (machine-ls d od)
   (run (docker-machine ls)))
 
+(define (machine-create d od)
+  (let* ((value (default-machine-name d)))
+    (run (docker-machine create -driver virtualbox ,value))))
+
 (define (machine-start d od)
-  (let* ((value (or-str (oret:value d) "default"))
-         )
-    (run (docker-machine start ,value))
-     ))
+  (let* ((value (default-machine-name d)))
+    (run (docker-machine start ,value))))
 
 (define (machine-stop d od)
-  (let* ((value (or-str (oret:value d) "default"))
-         )
-    (run (docker-machine stop ,value))
-     ))
+  (let* ((value (default-machine-name d)))
+    (run (docker-machine stop ,value))))
+
+(define (machine-ip d od)
+  (let* ((value (default-machine-name d)))
+    (run (docker-machine ip ,value))))
 
 (define (machine-env d od)
-  (let* ((value (or-str (oret:value d) "default"))
-         )
+  (let* ((value (default-machine-name d)))
     (run (docker-machine env ,value))
-     (cout "# eval \"$(dk env)")
-     ))
+    (cout "# eval \"$(dk env)")))
 
 (define (compose-up d od)
   (run (docker-compose up)))
@@ -27,14 +32,10 @@
   (run (docker-compose build)))
 
 (define (docker-ps d od)
-  (let* ((value (oret:value d))
-         )
+  (let* ((value (oret:value d)))
     (if (string=? value "") 
       (run (docker ps))
-      (run (docker ps ,value))
-      )
-    )
-  )
+      (run (docker ps ,value)))))
 
 (define (docker-cid image)
   (let* ((line (run/string 
@@ -45,24 +46,30 @@
     (display line)
     cid))
 
+(define (docker-images d od)
+  (run (docker images)))
+
 (define (docker-build d od)
   (run (docker build)))
 
 (define (docker-stop d od)
   (let* ((value (oret:value d))
          (image (or-str value "yarec/ornginx"))
-         (cid (docker-cid image))
-         )
+         (cid (docker-cid image)))
     (cout cid)
-    ))
+    (run (docker stop ,cid))))
 
 (define (docker data oret-data)
   (get-opt 
     `(
       (--ls           ls     " machine ls                "  ,machine-ls)
+      (--create   create|s|t " machine create            "  ,machine-create)
       (--mstart   mstart|s|t " machine start             "  ,machine-start)
       (--mstop     mstop|s|t " machine stop              "  ,machine-stop)
+      (--ip           ip|s|t " machine ip                "  ,machine-ip)
+      (--env         env|s|t " machine env               "  ,machine-env)
       (----------- -      "                           "  ,-)
+      (--images     imgs|s|t " show images               "  ,docker-images)
       (--ps           ps|s|t " show containers           "  ,docker-ps)
       (--stop       stop|s|t " stop container            "  ,docker-stop)
       (--build         b|s|t " build image               "  ,docker-build)
